b5001505fcbd0d7e28104e2182f6d8f23c911e97
Penitto <penitto02@gmail.com>
2026-02-02 21:08:00 +0300

    added blank lines

--- features_transforms/tf_idf.py
+++ features_transforms/tf_idf.py
@@ -2,6 +2,7 @@ import numpy as np
 from typing import List
 from collections import defaultdict
 
+
 class TF_IDF_Transformer:
 
     def __init__(self):
@@ -10,9 +11,9 @@ class TF_IDF_Transformer:
     def fit(self, corpus: List[str]):
         self.power = len(corpus)
         for text in corpus:
-          for word in set(text.split()):
-            self.idf_dict[word] += 1
-    
+            for word in set(text.split()):
+                self.idf_dict[word] += 1
+
     def transform_text(self, text: str) -> List[float]:
         tf = defaultdict(int)
         words = text.split()
@@ -20,12 +21,14 @@ class TF_IDF_Transformer:
 
         # Counting TF (Term Frequency)
         for word in words:
-          tf[word] += 1
+            tf[word] += 1
 
         for key, word in enumerate(words):
             tf_idf[key] = tf[word] / len(words) * (np.log((self.power) / (self.idf_dict[word] + 1)) + 1)
+
         return tf_idf
-    
+
+
     def transform_corpus(self, corpus: List[str]) -> List[List[float]]:
         result = []
         for text in corpus:
